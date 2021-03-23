bcb65f5ae02948ac06da8d8c7e2a1d6b84b3f3de
Jung Mee Park <jpark1917@gmail.com>
2021-03-23 10:07:00 -0700

    Initial commit with data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# R workshop on GitHub #
+# stimulate and plot data #
+# jmpark@email.arizona.edu #
+# 2021-03-23 #
+
+# Simulate data from normal distribution #
+x <- rnorm(n=100)
+y <- 2 * x + rnorm(n=100, sd=0.1)
+
+# plot simulated data
+plot(x=x, y=y)
